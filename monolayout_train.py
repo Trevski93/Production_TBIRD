d419c035bd3c64a3f970f59cb568b49847aed573
Trevor <tmitchellgt@gmail.com>
2020-05-12 00:35:48 -0400

    test

--- monolayout_train.py
+++ monolayout_train.py
@@ -138,13 +138,13 @@ class Trainer:
         self.models["encoder"] = model.Encoder(18, self.height, self.width, pretrained=False).to(self.device) #test batch_size 
         if self.opt.type == "both":
             self.models["static_decoder"] = model.Decoder(
-                self.models["encoder"].resnet_encoder.num_ch_enc).to(self.device)
+                self.models["encoder"].resnet_encoder.num_ch_enc.cuda()).to(self.device)
             
             self.models["static_discr"] = model.Discriminator().to(self.device)
             self.models["dynamic_discr"] = model.Discriminator().to(self.device)
             
             self.models["dynamic_decoder"] = model.Decoder(
-                self.models["encoder"].resnet_encoder.num_ch_enc).to(self.device)
+                self.models["encoder"].resnet_encoder.num_ch_enc.cuda()).to(self.device)
         else:
             self.models["decoder"] = model.Decoder(self.models["encoder"].resnet_encoder.num_ch_enc).to(self.device)
             self.models["discriminator"] = model.Discriminator().to(self.device)

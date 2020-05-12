c9a504bbb2000e1f3625e71cf04fd5fb0edbaeed
Trevor <tmitchellgt@gmail.com>
2020-05-11 21:27:29 -0400

    .to(device) for models

--- monolayout_train.py
+++ monolayout_train.py
@@ -130,16 +130,16 @@ class Trainer:
         self.models["encoder"] = model.Encoder(18, self.height, self.width, pretrained=False) #test batch_size 
         if self.opt.type == "both":
             self.models["static_decoder"] = model.Decoder(
-                self.models["encoder"].resnet_encoder.num_ch_enc)
+                self.models["encoder"].resnet_encoder.num_ch_enc).to(device)
             
-            self.models["static_discr"] = model.Discriminator()
-            self.models["dynamic_discr"] = model.Discriminator()
+            self.models["static_discr"] = model.Discriminator().to(device)
+            self.models["dynamic_discr"] = model.Discriminator().to(device)
             
             self.models["dynamic_decoder"] = model.Decoder(
-                self.models["encoder"].resnet_encoder.num_ch_enc)
+                self.models["encoder"].resnet_encoder.num_ch_enc).to(device)
         else:
-            self.models["decoder"] = model.Decoder(self.models["encoder"].resnet_encoder.num_ch_enc)
-            self.models["discriminator"] = model.Discriminator()
+            self.models["decoder"] = model.Decoder(self.models["encoder"].resnet_encoder.num_ch_enc).to(device)
+            self.models["discriminator"] = model.Discriminator().to(device)
         
         for key in self.models.keys():
             if "discr" in key:
